f32b825f1d8f0bc4b07bd76e6a8bc6e943e5c99f
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-07-03 19:07:11 -0700

    Rewrite code

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    a = tuple_a + (0, 0)
-    b = tuple_b + (0, 0)
-    tuple_c = a[0] + b[0], a[1] + b[1]
-    return tuple_c
+    a = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    b = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+    c = [x + y for x, y in zip(a, b)]
+    return tuple(c[0:2])
 
