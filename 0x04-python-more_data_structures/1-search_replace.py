53b456dba6024cbed2289b49ec374c721c557ab0
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-07-06 17:31:16 -0700

    fix error

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    dup_list = my_list.copy()
-    for i in range(len(dup_list)):
-        dup_list[i] = replace if my_list[i] == search else my_list[i]
-    return (dup_list)
-
+    return [replace if search == n else n for n in my_list]
