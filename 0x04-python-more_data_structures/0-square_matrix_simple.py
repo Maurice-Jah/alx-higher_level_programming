e11fdeb3b9d381ae77788ca022c1de40df5b8688
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-07-06 18:09:32 -0700

    fix error in code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    response = []
-    for row in matrix:
-        sub_matrix = map(lambda num: num**2, row)
-        response.append(list(sub_matrix))
-        return response
+    squared = []
+    for line in matrix:
+        squared.append([c**2 for c in line])
+    return squared
