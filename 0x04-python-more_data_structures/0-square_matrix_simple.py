64a5847593a59f4767d4851f8bc5b83e2d467cc4
Maurice-Jah <mauricejahwizwid@gmail.com>
2022-07-06 17:26:31 -0700

    rewrite code

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    dup_matrix = matrix.copy()
-    for line in range(len(matrix)):
-        dup_matrix[line] = list(map((lambda x: x**2), matrix[line]))
-        return(dup_matrix)
+    response = []
+    for row in matrix:
+        sub_matrix = map(lambda num: num**2, row)
+        response.append(list(sub_matrix))
+        return response
