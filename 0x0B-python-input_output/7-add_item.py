9647fe69e5e189650c38a5bee4109db27c2038b0
Maurice <mauricejahwizwid@gmail.com>
2022-08-03 02:30:41 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,23 +1,19 @@
 #!/usr/bin/python3
-
-'''Task 07 - 7. Load, add, save'''
-
-
+"""Add item script."""
 import sys
 
-
-save = __import__('5-save_to_json_file').save_to_json_file
-load = __import__('6-load_from_json_file').load_from_json_file
-
-file = "add_item.json"
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    f = load(file)
+    lst = load_from_json_file("add_item.json")
 except:
-    f = []
+    lst = []
 
 argc = len(sys.argv)
-for i in range(1, argc):
-    f.append(sys.argv[i])
 
-save(f, file)
+if argc > 1:
+    for i in range(1, argc):
+        lst.append(sys.argv[i])
+
+save_to_json_file(lst, "add_item.json")
